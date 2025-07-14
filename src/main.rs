6f961d709a9e501bac2e4743d1119f965f397e5b
fedoraiver <cck1772416022@gmail.com>
2025-07-15 02:01:50 +0800

    feat:支持资源处理添加bevy_aseprite_ultra加载diamonds_ace卡牌切片
    -.gitignore新增忽略imported_assets
    -Cargo.toml添加bevy_aseprite_ultra及asset_processing相关可选依赖和功能
    -main.rs启用Processed资源模式添加AsepriteUltraPlugin和setup_card系统加载cards.aseprite中的diamonds_ace切片

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy_aseprite_ultra::prelude::*;
 
 fn main() {
     let mut app = App::new();
@@ -6,11 +7,14 @@ fn main() {
         DefaultPlugins
             .set(AssetPlugin {
                 watch_for_changes_override: Some(true),
+                mode: AssetMode::Processed,
                 ..Default::default()
             })
             .set(ImagePlugin::default_nearest()),
     );
+    app.add_plugins(AsepriteUltraPlugin);
     app.add_systems(Startup, setup_camera);
+    app.add_systems(Startup, setup_card);
     app.run();
 }
 
@@ -24,5 +28,17 @@ fn setup_camera(mut commands: Commands) {
             },
             ..OrthographicProjection::default_2d()
         }),
+    ));
+}
+
+fn setup_card(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn((
+        AseSlice {
+            name: "diamonds_ace".into(),
+            aseprite: asset_server.load("cards.aseprite"),
+        },
+        Sprite {
+            ..Default::default()
+        },
     ));
 }
