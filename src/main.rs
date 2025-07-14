f833357d9534fbf991b8f23def4a75d71028b11a
fedoraiver <cck1772416022@gmail.com>
2025-07-14 22:12:19 +0800

    feat:初始化Bevy应用,配置插件
    -启用资源热重载(watch_for_changes_override)
    -设置图像插件使用最近邻采样方式(default_nearest)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new();
+    app.add_plugins(
+        DefaultPlugins
+            .set(AssetPlugin {
+                watch_for_changes_override: Some(true),
+                ..Default::default()
+            })
+            .set(ImagePlugin::default_nearest()),
+    );
+    app.run();
 }
